0ec97bed2c5d4db7b2bf3e1c022c9cc39abe59f2
subir-ghosh-au26 <85132702+subir-ghosh-au26@users.noreply.github.com>
2024-12-26 15:52:32 +0000

    build

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const bodyParser = require('body-parser');
 
 
 app.use("^/$", (req, res, next) => {
-    fs.readFile(path.resolve("./build/index.html"), "utf-8", (err, data) => {
+    fs.readFile(path.resolve("./public/index.html"), "utf-8", (err, data) => {
         if (err) {
             console.log(err);
             return res.status(500).send("Check it please !")
